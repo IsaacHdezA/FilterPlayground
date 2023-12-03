6f29a28e3d83a48c6571b5109ac7a3710d14c166
IsaacHdezA <aldoisaachernandez@outlook.com>
2023-12-02 21:37:46 -0600

    [KUWAHARA]: Kuwahara filter working (only BN).

--- main.cpp
+++ main.cpp
@@ -9,41 +9,88 @@ using namespace std;
 
 using namespace cv;
 
-vector<Mat> getRegions(const int RADIUS, const Mat &src, int x, int y);
+struct Region {
+  Mat pixels;
+
+  double mean;
+  double stdDev;
+
+  void print() const {
+    cout << pixels             << '\n'
+         << "Mean: "           << mean   << '\n'
+         << "Std. Deviation: " << stdDev << '\n' << endl;
+  }
+};
+
+vector<Region> getRegions(const int RADIUS, const Mat &src, int x, int y);
+uchar getKuwaharaPixel(const vector<Region> &regions);
 
 int main(void) {
   const string IMG_PATH = "./res/",
-               IMG_NAME = "me",
+               IMG_NAME = "suncat",
                IMG_EXT  = ".jpg",
                IMG_FILENAME = IMG_PATH + IMG_NAME + IMG_EXT;
 
-  Mat inputImg = imread(IMG_FILENAME, IMREAD_GRAYSCALE);
+  Mat inputImg = imread(IMG_FILENAME, IMREAD_GRAYSCALE),
+      outputImg = Mat::zeros(inputImg.rows, inputImg.cols, CV_8UC1);
 
   // Algorithm parameters
   // RADIUS (a) = kernel radius
-  const int RADIUS = 10;
-  int x = 201, y = 201;
+  const int RADIUS = 5;
 
-  vector<Mat> regions = getRegions(RADIUS, inputImg, x, y);
+  vector<Region> regions{4};
+
+  // Iterate every pixel
+  for(int r = RADIUS; r < inputImg.rows - RADIUS; r++) {
+    uchar  *inputRow = (uchar *)  inputImg.ptr<uchar>(r),
+          *outputRow = (uchar *) outputImg.ptr<uchar>(r);
+
+    for(int c = RADIUS; c < inputImg.cols - RADIUS; c++) {
+      regions = getRegions(RADIUS, inputImg, c, r);
+      outputRow[c] = getKuwaharaPixel(regions);
+    }
+  }
+
+  imwrite("./output/" + IMG_NAME + "_r" + to_string(RADIUS) + IMG_EXT, outputImg);
 
   waitKey();
   return 0;
 }
 
-vector<Mat> getRegions(const int RADIUS, const Mat &src, int x, int y) {
+vector<Region> getRegions(const int RADIUS, const Mat &src, int x, int y) {
   if(
     ((x - RADIUS) < 0) ||
     ((y - RADIUS) < 0) ||
     ((x + RADIUS) > src.cols) ||
     ((y + RADIUS) > src.rows)
-  ) exit(EXIT_FAILURE);
+  ) {
+    cout << "Pixel indices out of bounds (" << x << ", " << y << ")" << endl;
+    exit(EXIT_FAILURE);
+  }
 
-  vector<Mat> regions{4};
+  vector<Region> regions{4};
 
-  regions[0] = src(Rect(Point(x - RADIUS, y - RADIUS), Point(         x,          y)));
-  regions[1] = src(Rect(Point(         x, y - RADIUS), Point(x + RADIUS,          y)));
-  regions[2] = src(Rect(Point(x - RADIUS, y + RADIUS), Point(         x,          y)));
-  regions[3] = src(Rect(Point(         x,          y), Point(x + RADIUS, y + RADIUS)));
+  regions[0].pixels = src(Rect(Point(x - RADIUS, y - RADIUS), Point(         x,          y)));
+  regions[1].pixels = src(Rect(Point(         x, y - RADIUS), Point(x + RADIUS,          y)));
+  regions[2].pixels = src(Rect(Point(x - RADIUS, y + RADIUS), Point(         x,          y)));
+  regions[3].pixels = src(Rect(Point(         x,          y), Point(x + RADIUS, y + RADIUS)));
+
+  Scalar mean, stdDev;
+  for(Region &region : regions) {
+    meanStdDev(region.pixels, mean, stdDev);
+    region.mean = mean[0];
+    region.stdDev = stdDev[0];
+  }
 
   return regions;
+}
+
+uchar getKuwaharaPixel(const vector<Region> &regions) {
+  double min = 0;
+
+  for(int i = 0; i < regions.size(); i++) {
+    if(regions[i].stdDev < regions[min].stdDev) min = i;
+  }
+
+  return (uchar) regions[min].mean;
 }
