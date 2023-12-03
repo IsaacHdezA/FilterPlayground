faf129c64767561d50aadb4111df73dcdd0d6c86
IsaacHdezA <aldoisaachernandez@outlook.com>
2023-12-02 22:06:56 -0600

    [KUWAHARA]: Refactor code. Kuwahara filter is now a class.

---
+++ kuwahara.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "kuwahara.hpp"
+
+cv::Mat Kuwahara::applyFilter(const cv::Mat &src, int RADIUS) {
+  cv::Mat output = cv::Mat::zeros(src.rows, src.cols, CV_8UC1);
+  Kuwahara proxy;
+
+  std::vector<Region> regions{4};
+
+  // Iterate every pixel
+  for(int r = RADIUS; r < src.rows - RADIUS; r++) {
+    uchar *outputRow = (uchar *) output.ptr<uchar>(r);
+
+    for(int c = RADIUS; c < src.cols - RADIUS; c++) {
+      regions = proxy.getRegions(RADIUS, src, c, r);
+      outputRow[c] = proxy.getKuwaharaPixel(regions);
+    }
+  }
+
+  return output;
+}
+
+std::vector<Region> Kuwahara::getRegions(const int RADIUS, const cv::Mat &src, int x, int y) {
+  if(
+    ((x - RADIUS) < 0) ||
+    ((y - RADIUS) < 0) ||
+    ((x + RADIUS) > src.cols) ||
+    ((y + RADIUS) > src.rows)
+  ) {
+    std::cout << "Pixel indices out of bounds (" << x << ", " << y << ")" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  std::vector<Region> regions{4};
+
+  regions[0].pixels = src(cv::Rect(cv::Point(x - RADIUS, y - RADIUS), cv::Point(         x,          y)));
+  regions[1].pixels = src(cv::Rect(cv::Point(         x, y - RADIUS), cv::Point(x + RADIUS,          y)));
+  regions[2].pixels = src(cv::Rect(cv::Point(x - RADIUS, y + RADIUS), cv::Point(         x,          y)));
+  regions[3].pixels = src(cv::Rect(cv::Point(         x,          y), cv::Point(x + RADIUS, y + RADIUS)));
+
+  cv::Scalar mean, stdDev;
+  for(Region &region : regions) {
+    meanStdDev(region.pixels, mean, stdDev);
+    region.mean = mean[0];
+    region.stdDev = stdDev[0];
+  }
+
+  return regions;
+}
+
+uchar Kuwahara::getKuwaharaPixel(const std::vector<Region> &regions) {
+  double min = 0;
+
+  for(int i = 0; i < (int) regions.size(); i++) {
+    if(regions[i].stdDev < regions[min].stdDev) min = i;
+  }
+
+  return (uchar) regions[min].mean;
+}
+
---
+++ kuwahara.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "opencv2/core/core.hpp"
+#include <vector>
+
+struct Region {
+  cv::Mat pixels;
+
+  double mean;
+  double stdDev;
+};
+
+class Kuwahara {
+  public:
+    static cv::Mat applyFilter(const cv::Mat &src, int RADIUS = 3);
+
+  private:
+    std::vector<Region> getRegions(const int RADIUS, const cv::Mat &src, int x, int y);
+    uchar getKuwaharaPixel(const std::vector<Region> &regions);
+};
