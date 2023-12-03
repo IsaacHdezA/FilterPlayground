7395716fd9c6a069f4fdffcd1285cddd60c83df8
IsaacHdezA <aldoisaachernandez@outlook.com>
2023-12-03 01:49:04 -0600

    [KUWAHARA]: Initial color images processing. Needs refactor.

--- main.cpp
+++ main.cpp
@@ -11,23 +11,44 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char *argv[]) {
-  if(argc < 2 || argc > 4) {
-    cerr << "Incorrect usage. Usage is..." << endl;
-    cerr << "\tkuwuhara <inputImg> <radius> <outputImg>" << endl;
+  // if(argc < 2 || argc > 4) {
+  //   cerr << "Incorrect usage. Usage is..." << endl;
+  //   cerr << "\tkuwuhara <inputImg> <radius> <outputImg>" << endl;
 
-    exit(EXIT_FAILURE);
-  }
+  //   exit(EXIT_FAILURE);
+  // }
 
-  const string OUTPUT_PATH = "../../output/r" + to_string(atoi(argv[2])) + "_" + string(argv[3]);
+  // const string OUTPUT_PATH = "../../output/r" + to_string(atoi(argv[2])) + "_" + string(argv[3]);
 
-  cout << "Input image path: " << argv[1]     << '\n'
-       << "Kernel radius:    " << argv[2]     << '\n'
-       << "Output image:     " << OUTPUT_PATH << '\n' << endl;
+  // cout << "Input image path: " << argv[1] << '\n'
+  //      << "Kernel radius:    " << argv[2] << '\n'
+  //      << "Output image:     " << OUTPUT_PATH << '\n' << endl;
 
-  Mat inputImg = imread(argv[1], CV_8UC1),
-      outputImg = Kuwahara::applyFilter(inputImg, atoi(argv[2]));
+  // Mat inputImg = imread(argv[1], CV_8UC1),
+  //     outputImg = Kuwahara::applyFilter(inputImg, atoi(argv[2]));
 
-  imwrite(OUTPUT_PATH, outputImg);
+  // imwrite(OUTPUT_PATH, outputImg);
+
+  const int RADIUS = 10;
+
+  const string IMG_PATH = "./res/",
+               IMG_NAME = "mano",
+               IMG_EXT = ".jpg",
+               IMG_FILENAME = IMG_PATH + IMG_NAME + IMG_EXT;
+
+  cout << IMG_FILENAME << endl;
+  Mat inputImg = imread(IMG_FILENAME, IMREAD_COLOR),
+      outputImg;
+
+  cvtColor(inputImg, outputImg, COLOR_BGR2HSV);
+
+  vector<Mat> channels{3};
+  split(outputImg, channels);
+  channels[2] = Kuwahara::applyFilter(channels[2], RADIUS);
+  merge(channels, outputImg);
+  cvtColor(outputImg, outputImg, COLOR_HSV2BGR);
+
+  imwrite("./output/" + IMG_NAME + "_r" + to_string(RADIUS) + ".jpg", outputImg);
 
   waitKey();
   return 0;
