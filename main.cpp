10256b2493cc6021acd9cd2a280ca20469264949
IsaacHdezA <aldoisaachernandez@outlook.com>
2023-12-02 20:47:23 -0600

    [REGIONS]: GetRegions working.

--- main.cpp
+++ main.cpp
@@ -9,16 +9,41 @@ using namespace std;
 
 using namespace cv;
 
+vector<Mat> getRegions(const int RADIUS, const Mat &src, int x, int y);
+
 int main(void) {
-    const string IMG_PATH = "./res/",
-                 IMG_NAME = "me",
-                 IMG_EXT  = ".jpg",
-                 IMG_FILENAME = IMG_PATH + IMG_NAME + IMG_EXT;
+  const string IMG_PATH = "./res/",
+               IMG_NAME = "me",
+               IMG_EXT  = ".jpg",
+               IMG_FILENAME = IMG_PATH + IMG_NAME + IMG_EXT;
 
-    cout << "Input image path: " << IMG_FILENAME << endl;
-    Mat output = imread(IMG_FILENAME, IMREAD_GRAYSCALE);
-    imshow("hola", output);
+  Mat inputImg = imread(IMG_FILENAME, IMREAD_GRAYSCALE);
 
-    waitKey();
-    return 0;
+  // Algorithm parameters
+  // RADIUS (a) = kernel radius
+  const int RADIUS = 10;
+  int x = 201, y = 201;
+
+  vector<Mat> regions = getRegions(RADIUS, inputImg, x, y);
+
+  waitKey();
+  return 0;
+}
+
+vector<Mat> getRegions(const int RADIUS, const Mat &src, int x, int y) {
+  if(
+    ((x - RADIUS) < 0) ||
+    ((y - RADIUS) < 0) ||
+    ((x + RADIUS) > src.cols) ||
+    ((y + RADIUS) > src.rows)
+  ) exit(EXIT_FAILURE);
+
+  vector<Mat> regions{4};
+
+  regions[0] = src(Rect(Point(x - RADIUS, y - RADIUS), Point(         x,          y)));
+  regions[1] = src(Rect(Point(         x, y - RADIUS), Point(x + RADIUS,          y)));
+  regions[2] = src(Rect(Point(x - RADIUS, y + RADIUS), Point(         x,          y)));
+  regions[3] = src(Rect(Point(         x,          y), Point(x + RADIUS, y + RADIUS)));
+
+  return regions;
 }
