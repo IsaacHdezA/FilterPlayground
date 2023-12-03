75cbaa031d9f9078a7a7e41606b29db2a30799ec
IsaacHdezA <aldoisaachernandez@outlook.com>
2023-12-03 00:57:55 -0600

    [MAIN]: You can now use CLI arguments to specify parameters. Still needs work to do.

--- main.cpp
+++ main.cpp
@@ -10,18 +10,24 @@ using namespace std;
 
 using namespace cv;
 
-int main(void) {
-  const string IMG_PATH = "./res/",
-               IMG_NAME = "idaho",
-               IMG_EXT  = ".jpg",
-               IMG_FILENAME = IMG_PATH + IMG_NAME + IMG_EXT;
+int main(int argc, char *argv[]) {
+  if(argc < 2 || argc > 4) {
+    cerr << "Incorrect usage. Usage is..." << endl;
+    cerr << "\tkuwuhara <inputImg> <radius> <outputImg>" << endl;
 
-  const int RADIUS = 5;
+    exit(EXIT_FAILURE);
+  }
 
-  Mat inputImg = imread(IMG_FILENAME, IMREAD_GRAYSCALE),
-      outputImg = Kuwahara::applyFilter(inputImg, RADIUS);
+  const string OUTPUT_PATH = "../../output/r" + to_string(atoi(argv[2])) + "_" + string(argv[3]);
 
-  imwrite("./output/" + IMG_NAME + "_r" + to_string(RADIUS) + IMG_EXT, outputImg);
+  cout << "Input image path: " << argv[1]     << '\n'
+       << "Kernel radius:    " << argv[2]     << '\n'
+       << "Output image:     " << OUTPUT_PATH << '\n' << endl;
+
+  Mat inputImg = imread(argv[1], CV_8UC1),
+      outputImg = Kuwahara::applyFilter(inputImg, atoi(argv[2]));
+
+  imwrite(OUTPUT_PATH, outputImg);
 
   waitKey();
   return 0;
